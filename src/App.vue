ff8391d45e6d0d76b82d5efccd7e9dbd200363cc
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-18 17:04:24 +1000

    installing mark

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <h1>Markdown Preview</h1>
+  <input v-model.lazy="code" />
+  <p v-html="preview"></p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
+import marked from 'marked';
 
 export default defineComponent({
   name: 'App',
   components: {},
+  setup() {
+    const code = ref('');
+    const preview = computed(() => {
+      return marked(code.value);
+    });
+    return {
+      code,
+      preview,
+    };
+  },
 });
 </script>
 
